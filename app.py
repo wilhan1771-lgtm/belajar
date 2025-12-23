4c3917e1ecab6cbc5e01e7a514e4e60586ffbda2
William Hanitio <wilhan1771@gmail.com>
2025-12-23 17:12:37 +0700

    Tambah receiving: sampling + netto + tombol save

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import date
 
 app = Flask(__name__)
 app.secret_key = "belajar-secret"
 
 DEMO_USER = {"username": "admin", "password": "1234"}
+
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -17,7 +19,9 @@ def login():
             return redirect(url_for("dashboard"))
         else:
             error = "Username / password salah"
+
     return render_template("login.html", error=error)
+
 
 @app.route("/dashboard")
 def dashboard():
@@ -25,33 +29,38 @@ def dashboard():
         return redirect(url_for("login"))
     return render_template("dashboard.html", user=session["user"])
 
+
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("login"))
 
-# ✅ MENU ROUTES TARUH DI SINI (SEBELUM app.run)
-@app.route("/menu/<int:n>")
-def menu(n):
-    if "user" not in session:
-        return redirect(url_for("login"))
-    return f"Ini halaman Kotak {n} ✅"
 
 @app.route("/menu1")
 def menu1():
     return render_template("menu1.html")
 
+
 @app.route("/menu2")
 def menu2():
     return render_template("menu2.html")
+
 
 @app.route("/menu3")
 def menu3():
     return render_template("menu3.html")
 
+
 @app.route("/menu4")
 def menu4():
     return render_template("menu4.html")
+
+@app.route("/receiving")
+def receiving():
+    if "user" not in session:
+        return redirect(url_for("login"))
+    today = date.today().strftime("%d/%m/%Y")
+    return render_template("receiving.html", today=today)
 
 if __name__ == "__main__":
     app.run(debug=True)
